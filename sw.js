24fe4e25a48f1ab11b0bf487036efda7a6b62f07
EldarTichaud <lecomte.romain@yahoo.fr>
2026-05-18 22:12:08 +0200

    v0.6.5 — historique repas + édition patient + onglet données investigateur

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nsr-v064';
+const CACHE = 'nsr-v065';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
