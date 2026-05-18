68a13b9d70000a8b8f725bd28340269e5607d30d
EldarTichaud <lecomte.romain@yahoo.fr>
2026-05-18 23:36:25 +0200

    fix: repas invisibles après validation + NOVA correction hardcodée (v0.6.7)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nsr-v066';
+const CACHE = 'nsr-v067';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
