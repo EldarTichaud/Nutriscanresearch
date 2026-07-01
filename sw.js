6957bf428e42e5aa140ed6a1fae23a64171bae6b
EldarTichaud <lecomte.romain@yahoo.fr>
2026-07-01 22:50:44 +0200

    v0.7.2 — Signalement erreur de saisie patient + exclusion calculs investigateur

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nsr-v071';
+const CACHE = 'nsr-v072';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
