36064183247695343c9f31b8f0fcb2cda07f20fd
EldarTichaud <lecomte.romain@yahoo.fr>
2026-07-20 21:53:27 +0200

    v0.7.9 — Compliance : ajout métriques total projet + à jour (ancrées sur la durée réelle du protocole)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nsr-v078';
+const CACHE = 'nsr-v079';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
