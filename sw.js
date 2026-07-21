3732c7b75225c406dfdd75bdec332d878ca2a8f7
EldarTichaud <lecomte.romain@yahoo.fr>
2026-07-21 23:11:05 +0200

    afficher bilan au patient

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nsr-v084';
+const CACHE = 'nsr-v085';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
