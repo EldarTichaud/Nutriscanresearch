59e1f8c7b17dbcb416867e71f7137fd3bc7ceb28
EldarTichaud <lecomte.romain@yahoo.fr>
2026-07-21 22:13:42 +0200

    affichage nutritment sur résumé de saisie patient + possibilité de ne pas afficher ça

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nsr-v082';
+const CACHE = 'nsr-v084';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
