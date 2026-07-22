51b676ce701ba09c55fef22052cde439b93c5fc6
EldarTichaud <lecomte.romain@yahoo.fr>
2026-07-22 22:51:26 +0200

    historique

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nsr-v089';
+const CACHE = 'nsr-v090';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
